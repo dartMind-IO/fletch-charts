eee2faa835ff0ac87c1dcd5021121a6dc077f61a
Yczar <babalolagbogo@gmail.com>
2024-04-16 22:02:03 +0100

    chore: add fletch charts exports

--- lib/fletch_charts.dart
+++ lib/fletch_charts.dart
@@ -1,7 +1,12 @@
 library fletch_charts;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:fletch_charts/src/entities/charts/items/line_graph/line_graph.dart'
+    show LineGraph;
+export 'package:fletch_charts/src/entities/charts/items/bar_graph/bar_graph.dart'
+    show BarGraph;
+export 'package:fletch_charts/src/entities/charts/items/broken_sweep_chart/broken_sweep_chart.dart'
+    show BrokenSweepChart;
+export 'package:fletch_charts/src/entities/charts/items/selected_broken_sweep_chart/selected_broken_sweep_chart.dart'
+    show SelectedBrokenSweepChart;
+export 'package:fletch_charts/src/entities/charts/items/sweep_radial_chart/sweep_radial_chat.dart'
+    show SweepRadialChart;
