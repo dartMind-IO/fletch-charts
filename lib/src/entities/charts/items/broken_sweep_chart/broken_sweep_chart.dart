3d94e02e2ee3ab78a0d3c40754c23495b4c9385b
Yczar <babalolagbogo@gmail.com>
2024-04-17 09:52:30 +0100

    chore: update broken sweep chart

--- lib/src/entities/charts/items/broken_sweep_chart/broken_sweep_chart.dart
+++ lib/src/entities/charts/items/broken_sweep_chart/broken_sweep_chart.dart
@@ -1,15 +1,14 @@
 import 'dart:math' as math;
 
-import 'package:fletch_charts/src/entities/entities_barrel.dart';
+import 'package:fletch_charts/src/entities/app/theme/colors/app_colors.dart';
 import 'package:fletch_charts/src/tools/tools_barrel.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 
 part 'custom/painter.dart';
 
 part 'custom/text_gradient.dart';
 
-class BrokenSweepChart extends StatefulWidget {
+class BrokenSweepChart extends StatelessWidget {
   final Size? size;
 
   /// this is a list of values whose sum should be normalized between 0 and 1
@@ -22,71 +21,51 @@ class BrokenSweepChart extends StatefulWidget {
   /// it must be between zero and one as it is a fraction of 180º
   final double portionGap;
   final double thickness;
-  final double knobValue;
 
   const BrokenSweepChart({
     super.key,
     this.size,
-    required this.knobValue,
     required this.sweepPortionSizes,
     this.portionGap = 0.06,
     this.thickness = 15,
   });
 
   @override
-  State<BrokenSweepChart> createState() => _BrokenSweepChartState();
-}
-
-class _BrokenSweepChartState extends State<BrokenSweepChart> {
-  BuildContext? stackContext;
-
-  Offset? globalOffset;
-
-  void computePosition(BuildContext context) =>
-      SchedulerBinding.instance.addPostFrameCallback((_) {
-        if (stackContext != null) return;
-        stackContext = context;
-        final box = stackContext?.findRenderObject() as RenderBox?;
-        if (box == null) return;
-        final Offset offset = box.localToGlobal(Offset.zero);
-        globalOffset = offset;
-        setState(() {});
-      });
-
-  @override
   Widget build(BuildContext context) {
-    final Size painterSize = widget.size ?? Size(416, 213.27.doubled);
+    final Size painterSize = size ?? Size(416, 213.27.doubled);
     return LayoutBuilder(builder: (context, constraints) {
-      computePosition(context);
-      return Stack(
-        clipBehavior: Clip.none,
-        children: [
-          SizedBox.fromSize(
-            size: painterSize,
-            child: Transform.flip(
-              flipY: true,
-              flipX: true,
-              child: CustomPaint(
-                size: painterSize,
-                painter: BrokenSweepChartPainter(
-                  thickness: widget.thickness,
-                  sweepPortionSizes: widget.sweepPortionSizes,
-                  gap: widget.portionGap,
+      return Padding(
+        padding: EdgeInsets.all(thickness),
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            SizedBox.fromSize(
+              size: painterSize,
+              child: Transform.flip(
+                flipY: true,
+                flipX: true,
+                child: CustomPaint(
                   size: painterSize,
-                  gradientColors: [
-                    AppColors.gradient2,
-                    AppColors.gradient1,
-                  ],
+                  painter: BrokenSweepChartPainter(
+                    thickness: thickness,
+                    sweepPortionSizes: sweepPortionSizes,
+                    gap: portionGap,
+                    size: painterSize,
+                    gradientColors: [
+                      AppColors.gradient2,
+                      AppColors.gradient1,
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          SlidingKnob(
-            value: widget.knobValue,
-            diameter: 45.l,
-            parentSize: painterSize,
-          ),
-        ],
+            SlidingKnob(
+              value: 1,
+              diameter: 45.l,
+              parentSize: painterSize,
+            ),
+          ],
+        ),
       );
     });
   }
